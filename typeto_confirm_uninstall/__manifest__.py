99bf2e6473d752133b4bce70da5df1d277fd49c2
Angga Kawa <anggakawa@gmail.com>
2024-03-25 14:03:30 +0800

    init for odoo 16

---
+++ typeto_confirm_uninstall/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Type to Uninstall",
+    'version': '16.0',
+    "license": "LGPL-3",
+    "summary": "Add type to uninstall features to Odoo",
+    "description": """
+        This module introduces a safety mechanism to the Odoo uninstall process, preventing accidental or unintended module removals. 
+        Users must confirm their intention to uninstall by explicitly typing the module's technical name.
+    """,
+    "author": "anggakawa",
+    "website": "https://github.com/anggakawa/odoo-type-to-uninstall",
+    "depends": ["base"],
+    "data": [
+        'wizard/uninstall_form.xml',
+    ],
+    "installable": True,
+}
